0d8210d6c629aa9478a5b6737e81d11936b7e16a
giampaolo <giampaolo@a13ad8ac-ce12-0410-a602-c8c22c8f6e20>
2010-07-05 08:44:38 +0000

    git-svn-id: http://dev.inghist.nl/svn/BiografischPortaal/biodes/trunk@16845 a13ad8ac-ce12-0410-a602-c8c22c8f6e20

--- trunk/biodes/__init__.py
+++
@@ -1,2 +0,0 @@
-from biodes10 import *
-
--- trunk/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(name = 'biodes',
-      version = '2.0.0',
-      packages = ['biodes'],
-      install_requires = ['plone.memoize',
-                          ]
-      )
-
