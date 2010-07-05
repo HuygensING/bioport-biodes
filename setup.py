345089f997ef70acd76958254db45fd3922a6dd4
giampaolo <giampaolo@a13ad8ac-ce12-0410-a602-c8c22c8f6e20>
2010-07-05 10:45:09 +0000

    remove useless xml files
    git-svn-id: http://dev.inghist.nl/svn/BiografischPortaal/biodes/trunk@16858 a13ad8ac-ce12-0410-a602-c8c22c8f6e20

--- setup.py
+++ setup.py
@@ -3,7 +3,5 @@ from setuptools import setup
 setup(name = 'biodes',
       version = '2.0.0',
       packages = ['biodes'],
-      install_requires = ['plone.memoize',
-                          ]
       )
 
