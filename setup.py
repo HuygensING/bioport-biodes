d1f5dd6565689ded51fe3db2b932f73f14832bbb
jelle <jelle@a13ad8ac-ce12-0410-a602-c8c22c8f6e20>
2010-09-07 13:17:34 +0000

    git-svn-id: http://dev.inghist.nl/svn/BiografischPortaal/biodes/trunk@17838 a13ad8ac-ce12-0410-a602-c8c22c8f6e20

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name = 'biodes',
-      version = '2.0.0',
+      version = '1.1.0dev',
       packages = ['biodes'],
       requires = ["plone.memoize"],
       )
