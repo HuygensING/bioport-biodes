80161cc3b94f5fe5db536e20ee2e0c173782b028
giampaolo <giampaolo@a13ad8ac-ce12-0410-a602-c8c22c8f6e20>
2010-09-20 13:18:45 +0000

    fix dependancies in setup.py
    git-svn-id: http://dev.inghist.nl/svn/BiografischPortaal/biodes/trunk@18212 a13ad8ac-ce12-0410-a602-c8c22c8f6e20

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup
 setup(name = 'biodes',
       version = '1.1.1dev',
       packages = ['biodes'],
-      requires = ["plone.memoize"],
+      requires = ["plone.memoize",
+                  "names",
+                 ],
       )
 
